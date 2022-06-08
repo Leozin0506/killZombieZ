1521bd0ae793fe4441052b74a28cb395fd235a17
Leozin0506 <94076803+Leozin0506@users.noreply.github.com>
2022-06-08 16:44:40 -0300

    Add files via upload

---
+++ PRO-V2-AtireNoZumbi-1-main/PRO-V2-AtireNoZumbi-1-main/sketch.js
@@ -0,0 +1,53 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var edges;
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+  bg = createSprite(displayWidth/2-20, displayHeight/2-40, 20, 20);
+  bg.addImage(bgImg);
+  bg.scale = 1.1;
+
+  player = createSprite(displayWidth-1150, displayHeight-300,50,50);
+  player.addImage(shooterImg);
+  player.scale = 0.3;
+  player.setCollider("rectangle",0,0,300,500);
+  player.debug = true;
+  
+  edges = createEdgeSprites();
+
+}
+
+function draw() {
+  background(0); 
+
+
+  if(keyDown("UP_ARROW") || touches.length>0){
+    player.y = player.y-30;
+  }
+  if(keyDown("DOWN_ARROW") || touches.length>0){
+    player.y = player.y+30;
+  }
+
+  if(keyWentDown("SPACE")){
+    player.addImage(shooter_shooting)
+  }
+  if(keyWentUp("SPACE")){
+    player.addImage(shooterImg)
+  }
+
+  player.collide(edges[2]);
+  player.collide(edges[3]);
+drawSprites();
+
+}
